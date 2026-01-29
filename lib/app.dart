d0cf4099018553ffc4ce72d3b11b8a8356c236e3
dev-ajijul-islam <devajijulislm@gmail.com>
2026-01-29 21:41:39 +0600

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:module_27_live_test/screens/home_screen.dart';
+
+class LiveTest extends StatelessWidget {
+  const LiveTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
